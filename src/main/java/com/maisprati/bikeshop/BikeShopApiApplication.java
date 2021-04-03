09f2d02e4338aaeb443999edc985a802d69be53a
FelipeSimoes1 <ssimoes.felipe@gmail.com>
2021-04-03 11:00:07 -0300

    adicionando arquivos iniciais

---
+++ src/main/java/com/maisprati/bikeshop/BikeShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.maisprati.bikeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeShopApiApplication.class, args);
+	}
+
+}
